22392822ba3fe9e067d20821e5443ed94f9da0c3
Matthew Heffernan <mrhheffernan@gmail.com>
2026-06-16 22:32:58 -0700

    Load in simple room config

--- bookrs/src/main.rs
+++ bookrs/src/main.rs
@@ -1,3 +1,31 @@
+use serde::Deserialize;
+use std::fs;
+use toml::de::from_str;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    hotel: Hotel,
+}
+#[derive(Deserialize, Debug)]
+struct Hotel {
+    name: String,
+    rooms: Vec<Rooms>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Rooms {
+    room_type: String,
+    count: u32,
+}
+
+fn load_hotel() -> Config {
+    // Load config from file
+    let file = fs::read_to_string("src/config/hotel.toml").expect("Could not open file");
+    let hotel_config: Config = from_str(&file).unwrap();
+    hotel_config
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = load_hotel();
+    println!("{:?}", config);
 }
