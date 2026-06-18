bbd66da07a8f2da5cec4a1713d9dbb5e180fdbce
Matthew Heffernan <mrhheffernan@gmail.com>
2026-06-17 23:42:20 -0700

    Basic room assignment loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::io;
 use toml::de::from_str;
@@ -65,6 +65,78 @@ fn setup_calendar(config: Config) -> BookingCalendar {
     BookingCalendar { calendar: calendar }
 }
 
+fn allocate_room(calendar: &mut BookingCalendar, room_number: &u32, start_day: &u32, n_days: &u32) {
+    let end_day = start_day + n_days; // will need actual datetime handling in the future
+
+    for day in *start_day..end_day {
+        // Before each booking, assert that the room is available
+        let room_available: bool = calendar
+            .calendar
+            .get_mut(&day.to_string())
+            .unwrap()
+            .get_mut(&room_number.to_string())
+            .unwrap()
+            .available;
+
+        assert!(room_available);
+
+        calendar
+            .calendar
+            .get_mut(&day.to_string())
+            .unwrap()
+            .get_mut(&room_number.to_string())
+            .unwrap()
+            .available = false;
+
+        let room_available_after: bool = calendar
+            .calendar
+            .get_mut(&day.to_string())
+            .unwrap()
+            .get_mut(&room_number.to_string())
+            .unwrap()
+            .available;
+
+        assert_eq!(room_available_after, false);
+    }
+}
+
+fn search_rooms(
+    calendar: &mut BookingCalendar,
+    room_type: &String,
+    start_day: &u32,
+    n_days: &u32,
+) -> HashSet<String> {
+    let end_day = start_day + n_days; // will need actual datetime handling in the future
+
+    let mut available_rooms = HashSet::new();
+    // initialize available rooms with all possible room ids
+    for room_number in calendar
+        .calendar
+        .get(&start_day.to_string())
+        .unwrap()
+        .keys()
+    {
+        available_rooms.insert(room_number.to_string());
+    }
+
+    for day in *start_day..end_day {
+        let rooms_to_check = calendar.calendar.get_mut(&day.to_string()).unwrap();
+        let mut day_available_rooms = HashSet::new();
+        for key in rooms_to_check.keys() {
+            let room_to_check = rooms_to_check.get(key).unwrap();
+
+            if room_to_check.available && room_to_check.room_type == *room_type {
+                day_available_rooms.insert(key.to_string());
+            }
+        }
+        available_rooms = available_rooms
+            .intersection(&day_available_rooms)
+            .cloned()
+            .collect::<HashSet<_>>();
+    }
+    available_rooms
+}
+
 fn main() {
     let config = load_hotel();
     // println!("{:?}", config);
@@ -112,7 +184,7 @@ fn main() {
     // 5. After these basic pieces are implemented, take a step back and design an MVP system as well
     //    as what a north star system would look like.
 
-    while true {
+    loop {
         println!("Starting day?");
         let mut start_day = String::new();
         let stdin = io::stdin();
@@ -127,6 +199,7 @@ fn main() {
         let mut room_type_str = String::new();
         let stdin = io::stdin();
         let _ = stdin.read_line(&mut room_type_str);
+        room_type_str = room_type_str.trim_end().to_string();
         // todo: Add validation of room type against the config, or rather present some options in this prompt.
 
         println!(
@@ -134,6 +207,22 @@ fn main() {
             room_type_str.trim_end(),
             n_day.trim_end(),
             start_day.trim_end()
-        )
+        );
+
+        let start_day_int = start_day.trim_end().parse::<u32>().unwrap();
+        let n_day_int = n_day.trim_end().parse::<u32>().unwrap();
+
+        let available_rooms =
+            search_rooms(&mut calendar, &room_type_str, &start_day_int, &n_day_int);
+
+        let selected_room = available_rooms.iter().next().unwrap();
+        println!("Available rooms are {:?}", available_rooms);
+        println!("Selected room {}", selected_room);
+        allocate_room(
+            &mut calendar,
+            &selected_room.parse::<u32>().unwrap(),
+            &start_day_int,
+            &n_day_int,
+        );
     }
 }
